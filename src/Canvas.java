c8a9c2d086852cc9c66c19570d8e0da13dca3630
BuildTools <unconfigured@null.spigotmc.org>
2021-02-19 18:06:18 -0700

    Fixed bugs and cleaned up code.

--- src/Canvas.java
+++ src/Canvas.java
@@ -2,148 +2,193 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.concurrent.TimeUnit;
+
 public class Canvas extends JFrame {
 
+    //Spacing constrains for graphics
     public int mx = -100;
     public int my = -100;
-    boolean cells[][] = new boolean[40][20];
     int spacing = 1;
-    static int running = 0;
+
+    //Game of life cell grid
+    boolean cells[][] = new boolean[40][20];
+
+    //Boolean that indicates if the simulation is running
+    static volatile boolean running = false;
+    //Thread to update game of life cells
+    public Thread updater;
+
     public Canvas() {
         setSize(1440, 900);
         setUpButtons();
-        Grid grid=new Grid();
+        Grid grid = new Grid();
         this.add(grid);
         PaintCell paintCell = new PaintCell();
         this.addMouseListener(paintCell);
 
-        for (int x = 0; x < 40; x ++) {
-            for (int y = 0; y <20; y ++) {
+        for (int x = 0; x < 40; x++) {
+            for (int y = 0; y < 20; y++) {
                 cells[x][y] = false;
             }
         }
+        //This thread updates the game of life grid once every second
+        updater = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true) {
+                    if (running) {
+                        generateNewCells();
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+
+        });
+
+        updater.start();
+
         setVisible(true);
     }
-
-    public void generateNewCells(){
-        System.out.println("Next Generation");
-        boolean [][] nextGrid = new boolean[cells.length][cells[0].length];
-        int [][] dirs = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,0},{0,1},{1,-1},{1,0},{1,1},};
-        for (int i = 1; i < cells.length - 1; i++)
-        {
-            for (int j = 1; j < cells[0].length - 1; j++)
-            {
+    /*
+    This function applies the rules of the game of life to the cell grid is responsible for changing a cells "alive"
+    or "dead" status.
+     */
+    public void generateNewCells() {
+        boolean[][] nextGrid = new boolean[cells.length][cells[0].length];
+        //Cell neighbors
+        int[][] dirs = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 0}, {0, 1}, {1, -1}, {1, 0}, {1, 1},};
+        //Loop through all cells and count their neighbors
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[0].length; j++) {
                 int neighboursTrue = 0;
-
-                for(int [] dir : dirs) {
-                    if (cells[i + dir[1]][j + dir[0]]) {
-                        neighboursTrue++;
+                //Count cell neighbors
+                for (int[] dir : dirs) {
+                    if (i + dir[1] >= 0 && i + dir[1] < 40 && j + dir[0] >= 0 && j + dir[0] < 20) {
+                        if (cells[i + dir[1]][j + dir[0]]) {
+                            neighboursTrue++;
+                        }
                     }
                 }
-                if(cells[i][j]){
+                //A cell cannot be its own neighbor
+                if (cells[i][j]) {
                     neighboursTrue--;
                 }
-                if (cells[i][j] && neighboursTrue < 2){
+                //Game of life rules
+                if (cells[i][j] && neighboursTrue < 2) {
                     nextGrid[i][j] = false;
-                } else if (cells[i][j] && neighboursTrue >= 2){
+                } else if (cells[i][j] && neighboursTrue >= 4) {
                     nextGrid[i][j] = false;
                 } else if (!cells[i][j] && neighboursTrue == 3) {
                     nextGrid[i][j] = true;
-                } else{
+                } else {
                     nextGrid[i][j] = cells[i][j];
                 }
 
             }
         }
-
-        for (int i = 1; i < cells.length - 1; i++)
-        {
-            for (int j = 1; j < cells[0].length - 1; j++)
-            {
+        //Update grid with next generation of cells
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[0].length; j++) {
                 cells[i][j] = nextGrid[i][j];
             }
         }
+        //Rerender the grid
+        repaint();
     }
 
-    public void setUpButtons(){
+    /*
+    This method adds the buttons the the main panel and gives them control over starting and stopping the game
+     */
+    public void setUpButtons() {
+        //Create the buttons
         JPanel buttonPanel = new JPanel();
         buttonPanel.setPreferredSize(new Dimension(1440, 100));
         JButton startPauseButton = new JButton("Start");
         JButton resetButton = new JButton("Reset");
 
+        //When the Start button is clicked
         startPauseButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(startPauseButton.getText().startsWith("S")) {
+                if (startPauseButton.getText().startsWith("S")) {
                     startPauseButton.setText("Pause");
-                    System.out.println("Start has been clicked!");
-                    running=1;
-
-                }else {
+                    running = true;
+                } else {
                     startPauseButton.setText("Start");
-                    running=0;
-                    System.out.println("Pause has been clicked!");
-
+                    running = false;
                 }
             }
         });
 
+        //When the reset button is clicked
         resetButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 startPauseButton.setText("Start");
-                System.out.println("Reset has been clicked!");
-                new Canvas();
-                running=0;
+                running = false;
+                for (int x = 0; x < 40; x++) {
+                    for (int y = 0; y < 20; y++) {
+                        cells[x][y] = false;
+                    }
+                }
+                repaint();
             }
         });
 
+        //Add buttons to the main panel
         buttonPanel.add(startPauseButton);
         buttonPanel.add(resetButton);
         this.add(buttonPanel, BorderLayout.PAGE_END);
     }
 
+    /*
+    This class handles the rendering for the grid.
+     */
     public class Grid extends JPanel {
 
         public void paint(Graphics g) {
+            //Draw grid background
             g.setColor(Color.DARK_GRAY);
-            g.fillRect(0,30,1200,600);
+            g.fillRect(0, 30, 1200, 600);
+            //Loop through all cells
             for (int x = 0; x < 40; x++) {
-                for (int y = 0; y < 20; y ++) {
+                for (int y = 0; y < 20; y++) {
+                    //Only draw yellow cells if they are alive
+                    if (cells[x][y]) {
+                        g.setColor(Color.yellow);
+                        g.fillRect(spacing + x * 30, spacing + y * 30 + 30, 30 - 2 * spacing, 30 - 2 * spacing);
+                    }
+                    //Draw grid
                     g.setColor(Color.gray);
-                    if (mx >= spacing+x*30 && mx < x*30+30-spacing && my>=spacing+y*30+30+26 && my<spacing+y*30+26+30+30-2*spacing) {
-                        g.setColor(Color.red);
-                        g.fillRect(spacing+x*30, spacing+y*30+30, 30-2*spacing, 30-2*spacing);
-                        cells[x][y]=!cells[x][y];
-                    }
-                    if(cells[x][y]){
-                        g.setColor(Color.red);
-                        g.fillRect(spacing+x*30, spacing+y*30+30, 30-2*spacing, 30-2*spacing);
-                    }
-                    g.drawRect(spacing+x*30, spacing+y*30+30, 30-2*spacing, 30-2*spacing);
+                    g.drawRect(spacing + x * 30, spacing + y * 30 + 30, 30 - 2 * spacing, 30 - 2 * spacing);
                 }
-            }
-            if(running==1){
-                generateNewCells();
-                repaint();
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            } else{
-                repaint();
             }
         }
     }
 
+    /*
+    This class listens for when the user clicks on a cell in the grid
+     */
     public class PaintCell implements MouseListener {
 
         @Override
-        public void mouseClicked(MouseEvent e){
-            System.out.println("Mouse clicked");
-            mx=e.getX();
-            my=e.getY();
+        public void mouseClicked(MouseEvent e) {
+            mx = e.getX();
+            my = e.getY();
+            //Transforms mouse click coordinates into game of life grid coordinates
+            int gridX = (e.getX() - spacing) / 30;
+            int gridY = (e.getY() - spacing - 60) / 30;
+            if (gridX < 40 && gridY < 20) {
+                //Set selected cell to "alive" status
+                cells[gridX][gridY] = !cells[gridX][gridY];
+            }
+            //Rerender the grid with updated cell information
             repaint();
         }
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
-
+/*
+This program implements the Game of Life
+@author Joshua Marriott, Ria Mehta, Siddharth Uppal
+ */
 public class Main {
     public static void main( String args[] ) {
         Canvas application = new Canvas();
